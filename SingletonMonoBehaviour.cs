f33513d197e3d30ee75d0cae1e4e3cdc5dc6e00d
Kohei Imatomi <ondatyan@gmail.com>
2017-02-05 11:14:50 +0900

    SingletonMonoBehaviour クラスの追加

---
+++ SingletonMonoBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Type t = typeof(T);
+
+                instance = (T)FindObjectOfType(t);
+                if (instance == null)
+                {
+                    Debug.LogErrorFormat("{0} をアタッチしているGameObjectはありません", t);
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    virtual protected void Awake()
+    {
+        if (this != Instance)
+        {
+            Destroy(this);
+
+            Debug.LogErrorFormat("{0} は既に他のGameObjectにアタッチされているため、コンポーネントを破棄しました. アタッチされているGameObjectは {1} です.", typeof(T), Instance.gameObject.name);
+
+            return;
+        }
+
+        // シーンを跨いでこのオブジェクトが消されないようにする
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
