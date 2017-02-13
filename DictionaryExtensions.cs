a60a58ee416aa0e3cca1dfb426fe7edcd79455a3
Kohei Imatomi <ondatyan@gmail.com>
2017-02-13 20:14:22 +0900

    DictionaryExtensions の追加

---
+++ DictionaryExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+public static class DictionaryExtensions
+{
+    public static V Get<K, V>(this Dictionary<K, V> self, K key, V defaultValue = default(V))
+    {
+        V value;
+        return self.TryGetValue(key, out value) ? value : defaultValue;
+    }
+}
