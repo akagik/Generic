eff18ce79bec764b21fd353c6bce88e59888d3b8
Kohei Imatomi <ondatyan@gmail.com>
2017-03-09 17:54:36 +0900

    MsgPack を Script に追加

---
+++ Scripts/MsgPack/Packable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MsgPack
+{
+    // Packable を実装したクラスは引数なしコンストラクタを実装する必要がある
+    interface Packable
+    {
+        void Pack(ObjectPacker packer, MsgPackWriter writer);
+        object Unpack(ObjectPacker packer, MsgPackReader reader);
+    }
+}
