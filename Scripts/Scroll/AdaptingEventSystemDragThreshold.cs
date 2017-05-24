63d97a8a8e6c1ecd15e9d8e26789d12dc0485696
Kohei Imatomi <ondatyan@gmail.com>
2017-05-25 00:03:04 +0900

    スクロールレクトがモバイル端末でタップしにくい問題を解決

---
+++ Scripts/Scroll/AdaptingEventSystemDragThreshold.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class AdaptingEventSystemDragThreshold : MonoBehaviour
+{
+    [SerializeField]
+    private EventSystem eventSystem;
+    [SerializeField]
+    private float inchDistance = 0.08f;
+
+    void Awake()
+    {
+        UpdatePixelDrag();
+    }
+
+    public void UpdatePixelDrag()
+    {
+        if (eventSystem == null)
+        {
+            Debug.LogWarning("Trying to set pixel drag for adapting to screen dpi, " +
+                           "but there is no event system assigned to the script", this);
+        }
+        eventSystem.pixelDragThreshold = Mathf.RoundToInt(ScreenUtils.dpi * inchDistance);
+    }
+}
---
+++ Utils/ScreenUtils.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class ScreenUtils
+{
+    public static float dpi
+    {
+        get
+        {
+#if UNITY_EDITOR
+            // Unity Editor の場合は端末の大きさを仮定して DPI を計算する
+            return 1080 / 2.64f;
+#else
+            return Screen.dpi;
+#endif
+        }
+    }
+}
