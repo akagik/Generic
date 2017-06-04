5309c456e24c38eca523680da8e80061da8f0e5c
Kohei Imatomi <ondatyan@gmail.com>
2017-06-04 21:16:57 +0900

    GenericUtils クラスを追加

---
+++ Scripts/GenericUtils.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+
+public static class GenericUtils
+{
+    public static Vector3 RandomVector3(float min, float max)
+    {
+        float x = UnityEngine.Random.Range(min, max);
+        float y = UnityEngine.Random.Range(min, max);
+        float z = UnityEngine.Random.Range(min, max);
+
+        return new Vector3(x, y, z);
+    }
+}
