d2297ced90740d57e1361e615f2ca42f44e0255e
Kohei Imatomi <ondatyan@gmail.com>
2017-06-07 21:37:49 +0900

    Swap と InsertSort の追加

--- Scripts/GenericUtils.cs
+++ Scripts/GenericUtils.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using System;
 
 public static class GenericUtils
 {
@@ -11,4 +11,31 @@ public static class GenericUtils
 
         return new Vector3(x, y, z);
     }
+
+    public static void Swap<T>(ref T lhs, ref T rhs)
+    {
+        T temp;
+        temp = lhs;
+        lhs = rhs;
+        rhs = temp;
+    }
+
+    public static void InsertSort<T>(T[] a) where T : IComparable<T>
+    {
+        int n = a.Length;
+        for (int i = 1; i < n; i++)
+            for (int j = i; j >= 1; --j)
+            {
+                if (a[j - 1] != null && a[j - 1].CompareTo(a[j]) <= 0)
+                {
+                    break;
+                }
+                else if (a[j] != null && a[j].CompareTo(a[j - 1]) > 0)
+                {
+                    break;
+                }
+                GenericUtils.Swap(ref a[j], ref a[j - 1]);
+            }
+    }
+
 }
