bd7121fef92dc5057801b16414b58933c17ec9ca
kohei <ondatyan@gmail.com>
2018-03-02 13:06:53 +0900

    Add HashSet.Choise method

--- Scripts/Extensions/HashSetExtensions.cs
+++ Scripts/Extensions/HashSetExtensions.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
 
 public static class HashSetExtensions
 {
-    public static T First<T>(this HashSet<T> set) {
+    public static T First<T>(this HashSet<T> set)
+    {
         foreach (T t in set)
         {
             return t;
         }
         throw new Exception("set must have at least one element.");
+    }
+
+    public static T Choice<T>(this HashSet<T> set)
+    {
+        int i = 0;
+        int target = Random.Range(0, set.Count);
+
+        foreach (T t in set)
+        {
+            if (i == target)
+            {
+                return t;
+            }
+            i++;
+        }
+        throw new Exception("Index is out of bound.");
     }
 }
