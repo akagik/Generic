66a5f48380f5c2af153b5b1128288d02ea5f15b4
Kohei Imatomi <kohei.imatomi@gmail.com>
2018-05-18 16:48:57 +0900

    Add ScriptableObjectCreator

---
+++ Editor/ScriptableObjectCreator.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using UnityEditor;
+
+namespace Generic
+{
+    public static class ScriptableObjectCreator
+    {
+        public static void Create<T>(T t, string name = "NewAsset") where T : UnityEngine.Object
+        {
+            string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+            if (path == "")
+            {
+                path = "Assets";
+            }
+            else if (Path.GetExtension(path) != "")
+            {
+                path = path.Replace(Path.GetFileName(path), "");
+            }
+
+            string filePath = Path.Combine(path, name + ".asset");
+            AssetDatabase.CreateAsset(t, AssetDatabase.GenerateUniqueAssetPath(filePath));
+            AssetDatabase.Refresh();
+        }
+    }
+}
