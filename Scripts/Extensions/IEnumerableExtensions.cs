4505355cb145d83d8c5cb3a4fea855bc1d999440
Kohei Imatomi <kohei.imatomi@gmail.com>
2018-06-20 22:18:07 +0900

    Add ForEach method

---
+++ Scripts/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+public static class IEnumerableExtensions {
+    public static void ForEach<T>(this IEnumerable<T> source, System.Action<T> action) {
+        foreach (T obj in source)
+            action(obj);
+    }
+}
