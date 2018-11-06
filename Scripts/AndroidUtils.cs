14187fcbc2339484ebbf09757ad9069c964470f3
Kohei Imatomi <kohei.imatomi@gmail.com>
2018-11-07 01:52:28 +0900

    AndroidUtils 実装

---
+++ Scripts/AndroidUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AndroidUtils
+{
+    public static string GetInternalStoragePath()
+    {
+        string path = "";
+        using(AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer"))
+        {
+            using(AndroidJavaObject currentActivity = jc.GetStatic<AndroidJavaObject>("currentActivity"))
+            {
+                path = currentActivity.Call<AndroidJavaObject>("getFilesDir").Call<string>("getCanonicalPath");
+            }
+        }
+        return path;
+    }
+}
