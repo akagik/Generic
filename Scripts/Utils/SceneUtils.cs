1a2e35d9416cb899bdcbb98c8a8360ccb8059bb7
Kohei Imatomi <kohei.imatomi@gmail.com>
2019-02-21 00:01:16 +0900

    SceneUtils クラスの追加

---
+++ Scripts/Utils/SceneUtils.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// シーンに関連したユーティリティクラス.
+/// </summary>
+public static class SceneUtils
+{
+    /// <summary>
+    /// 指定のシーンが読み込まれている場合は true を返す.
+    /// </summary>
+    public static bool IsSceneLoaded(string sceneName)
+    {
+        for(int i = 0; i < UnityEngine.SceneManagement.SceneManager.sceneCount; i++)
+        {
+            if(SceneManager.GetSceneAt(i).name == sceneName)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// 指定のシーンを読み込んで、指定の型の参照を取得してそれを返す.
+    /// 指定の型が見つかれば即座にそれを返す.
+    /// </summary>
+    public static IEnumerator WaitForSceneLoaded<T>(string sceneName) where T : UnityEngine.Object
+    {
+        T[] foundStates = GameObject.FindObjectsOfType<T>();
+
+        // 型T のオブジェクトが見つかればそれをセットする.
+        if(foundStates.Length > 0)
+        {
+            yield return foundStates[0];
+            yield break;
+        }
+
+        // 型T のオブジェクトが見つからなければシーンをロードする.
+        var op = SceneManager.LoadSceneAsync(sceneName,LoadSceneMode.Additive);
+        while(!op.isDone)
+        {
+            yield return null;
+        }
+
+        // currentState が取得できるまで繰り返す.
+        while(foundStates.Length == 0)
+        {
+            foundStates = GameObject.FindObjectsOfType<T>();
+        }
+        yield return foundStates[0];
+    }
+}
