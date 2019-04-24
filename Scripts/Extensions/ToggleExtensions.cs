afcddb8ffff5c90568d3d85233e33d10589c8c05
Kohei Imatomi <kohei.imatomi@gmail.com>
2019-04-24 21:48:52 +0900

    ToggleExtensions の追加

---
+++ Scripts/Extensions/ToggleExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class ToggleExtensions
+{
+    static Toggle.ToggleEvent emptyToggleEvent = new Toggle.ToggleEvent();
+
+    /// <summary>
+    /// イベントトリガーなしで isOn の値を変更する.
+    /// </summary>
+    public static void SetOnWithoutEvent(this Toggle toggle, bool isOn)
+    {
+        var originalEvent = toggle.onValueChanged;
+        toggle.onValueChanged = emptyToggleEvent;
+        toggle.isOn = isOn;
+        toggle.onValueChanged = originalEvent;
+    }
+}
