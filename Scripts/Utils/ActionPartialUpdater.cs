d5a54563a6d328b0e5ad176f849dca1ef615c71a
Kohei Imatomi <kohei.imatomi@gmail.com>
2020-06-13 15:05:59 +0900

    PartialUpdater を追加

---
+++ Scripts/Utils/ActionPartialUpdater.cs
@@ -0,0 +1,53 @@
+﻿namespace Generic
+{
+    using System;
+
+    public class ActionPartialUpdater
+    {
+        public Action<int> onUpdate { get; private set; }
+        public int total { get; private set; }
+        public int delta { get; private set; }
+
+        public int firstIndex { get; private set; }
+        public int lastIndex { get; private set; }
+
+        public ActionPartialUpdater(Action<int> onUpdate, int total, int delta = 1)
+        {
+            this.onUpdate = onUpdate;
+            this.total = total;
+            this.delta = delta;
+
+            if (delta <= 0)
+            {
+                throw new ArgumentException("delta must be greater than 0. Actual: " + delta);
+            }
+        }
+
+        public void OnUpdate()
+        {
+            firstIndex = lastIndex;
+            lastIndex = (firstIndex + delta) % total;
+
+            // 一周したとき
+            if (firstIndex > lastIndex)
+            {
+                for (int i = firstIndex; i < total; i++)
+                {
+                    onUpdate(i);
+                }
+
+                for (int i = 0; i < lastIndex; i++)
+                {
+                    onUpdate(i);
+                }
+            }
+            else
+            {
+                for (int i = firstIndex; i < lastIndex; i++)
+                {
+                    onUpdate(i);
+                }
+            }
+        }
+    }
+}
---
+++ Scripts/Utils/PartialUpdater.cs
@@ -0,0 +1,43 @@
+﻿namespace Generic
+{
+    using System;
+    using UnityEngine;
+
+    public class PartialUpdater
+    {
+        public int total { get; private set; }
+        public int delta { get; private set; }
+
+        // lastIndex < firstIndex となるような更新を許すか?
+        // false の場合、必ず一度は lastIndex == total となるような更新を行う.
+        public bool cyclicLastIndex = false;
+
+        public int firstIndex { get; private set; }
+        public int lastIndex { get; private set; }
+
+        public PartialUpdater(int total, int delta = 1)
+        {
+            this.total = total;
+            this.delta = delta;
+
+            if (delta <= 0)
+            {
+                throw new ArgumentException("delta must be greater than 0. Actual: " + delta);
+            }
+        }
+
+        public void OnUpdate()
+        {
+            firstIndex = lastIndex % total;
+
+            if (cyclicLastIndex)
+            {
+                lastIndex = (firstIndex + delta) % total;
+            }
+            else
+            {
+                lastIndex = Mathf.Min(firstIndex + delta, total);
+            }
+        }
+    }
+}
